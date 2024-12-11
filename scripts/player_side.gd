4bdbcca795f32018baa62e230c222fa1fbfc0735
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2024-12-11 21:51:34 +0200

    first pass

--- scripts/player_side.gd
+++ scripts/player_side.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
+var is_dying = false
 var is_jumping = false
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -325.0
 
-
+@onready var death_timer: Timer = $death_timer
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	add_to_group("player")
+	death_timer.connect("timeout", Callable(self, "_on_DeathTimer_timeout"))
 
 func _physics_process(delta: float) -> void:
+	
+	if is_dying:
+		return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -36,6 +42,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func update_animation(direction):
+	if is_dying:
+		return
 	if is_jumping:
 		animated_sprite_2d.play("jump")
 	elif direction != 0:
@@ -43,3 +51,39 @@ func update_animation(direction):
 		animated_sprite_2d.play("run")
 	else: 
 		animated_sprite_2d.play("idle")
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and body.is_alive:
+		die()
+		
+func die():
+	if is_dying:
+		return
+	is_dying = true
+	animated_sprite_2d.play("die")
+	death_timer.start(2.0)
+	move_player_up_and_down()
+	
+func move_player_up_and_down():
+	var start_position = position
+	var up_position = start_position + Vector2(0, -100)
+	var down_position = start_position + Vector2(0, 600)
+	
+	while position.y > up_position.y:
+		position.x -= 2
+		position.y -= 4
+		await get_tree().create_timer(0.01).timeout
+		
+	while position.y < down_position.y:
+		position.y += 4
+		await get_tree().create_timer(0.01).timeout
+
+func _on_DeathTimer_timeout():
+	Global.player_lives -= 1
+	if Global.player_lives > 0:
+		Global.enemies_killed = 0
+		get_tree().reload_current_scene()
+	else: 
+		Dialogic.VAR.Knowledge -= 0.3
+		get_tree().change_scene_to_file("res://scenes/maze.tscn")
--- scripts/toad.gd
+++ scripts/toad.gd
@@ -1,18 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 25
-
+var SPEED = -25
+var facing_right = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var is_alive = true
+var distance_travelled = 0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
+@export var distance_to_move = 40
+	
+func _ready() -> void:
+	add_to_group("enemy")
 	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	
-	velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
-	velocity.x = -SPEED
+	
+	if position.x == distance_to_move or endOfPlatform():
+		flip()
+		
+	position.x += SPEED * delta
+	distance_travelled += velocity.x
 	update_animation()
 	move_and_slide()
 
@@ -23,4 +35,19 @@ func update_animation():
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		is_alive = false
+		Global.enemies_killed += 1
 		queue_free()
+
+func flip():
+	distance_travelled = 0
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		SPEED = abs(SPEED)
+	else:
+		SPEED = abs(SPEED) * -1
+	
+	
+func endOfPlatform():
+	return !ray_cast_2d.is_colliding() && is_on_floor()
