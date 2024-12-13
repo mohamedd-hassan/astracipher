d8a0269bd822b98aa161e7e3517dd5cca794d3e3
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2024-12-13 18:13:35 +0200

    fixes

--- scripts/act_1.gd
+++ scripts/act_1.gd
@@ -25,4 +25,3 @@ func _on_level_spawn(destination_tag: String):
 	var door_path = "Door_" + destination_tag
 	var door = get_node(door_path) as Door
 	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
-
