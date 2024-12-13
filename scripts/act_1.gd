0d68ba9ed5644f499d61e21067b54054bb19ace4
Kuroi Yume <omarmail2004@gmail.com>
2024-12-13 17:39:44 +0200

    adding npcs

--- scripts/act_1.gd
+++ scripts/act_1.gd
@@ -1,9 +1,18 @@
 extends Node2D
+@onready var npc_3: Area2D = $npc3
+
+
+
 
 func _ready() -> void:
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _on_dialogic_signal(argument:String):
-	if argument == "end":
-		print("change")
-		get_tree().change_scene_to_file("res://scenes/maze.tscn")
+	if argument == "Ignored1":
+		npc_3.position+=Vector2(150,10)
+	if argument == "Ignored2":
+		npc_3.position+=Vector2(150,10)
+	if argument == "responded1":
+		npc_3.position+=Vector2(1500,10)
+	if argument == "responded2":
+		npc_3.position+=Vector2(1500,10)
