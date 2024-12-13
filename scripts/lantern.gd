6a9a95ad7cfc9fc49169e4490dbd4c8ad5e0ebf7
Nourin <nourinawad@gmail.com>
2024-12-13 16:10:21 +0200

    new maze tiles + torches lighting and animation

--- scripts/lantern.gd
+++ scripts/lantern.gd
@@ -1,10 +1,12 @@
 extends AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $PointLight2D/AnimationPlayer
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var lantern: AnimatedSprite2D = $"."
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
