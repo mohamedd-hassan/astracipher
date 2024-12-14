1e21b1c2d6d41f24ab1b1da980e46d4c89d234d8
Nourin <nourinawad@gmail.com>
2024-12-14 02:36:21 +0200

    astracipher dialogue & animation updates

--- scripts/maze.gd
+++ scripts/maze.gd
@@ -10,9 +10,11 @@ const KNOWLEDGE_VARIATION = 0.1
 @onready var player_animation: AnimatedSprite2D = $Player/AnimatedSprite2D2
 
 var met_cipher = false
+var animation_end = false
 
 func _ready() -> void:
 	player.global_position = spawn.global_position
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 
 func _process(delta: float) -> void:
@@ -20,6 +22,10 @@ func _process(delta: float) -> void:
 	if !met_cipher:
 		astra_animation.play("idle_down")
 	knowledge_light_animation.play("flicker")
+
+func _on_dialogic_signal(argument:String):
+	if argument == "move_forward":
+		cutscene_animation.play("move_forward")
 	
 func _on_interaction_area_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -29,4 +35,10 @@ func _on_interaction_area_body_entered(body: Node2D) -> void:
 	
 func cutscene():
 	print("should play animation")
+	player.set_physics_process(false)
+	player.set_process_input(false)
 	cutscene_animation.play("walk_to_astra")
+	await cutscene_animation.animation_finished
+	print("dialogue")
+	Dialogic.start("astracipher_timeline")
+	await Dialogic.timeline_ended
