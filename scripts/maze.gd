7b506d750079f28683feaecbfead479bb64faf0d
Nourin <nourinawad@gmail.com>
2024-12-14 19:04:37 +0200

    astracipher cutscene updates

--- scripts/maze.gd
+++ scripts/maze.gd
@@ -8,11 +8,13 @@ const KNOWLEDGE_VARIATION = 0.1
 @onready var cutscene_animation: AnimationPlayer = $CutsceneAnimation
 @onready var spawn: Marker2D = $Door_L/Spawn
 @onready var player_animation: AnimatedSprite2D = $Player/AnimatedSprite2D2
+@onready var drone: AnimatedSprite2D = $Drone
 
 var met_cipher = false
 var animation_end = false
 
 func _ready() -> void:
+	drone.visible = false
 	player.global_position = spawn.global_position
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
@@ -26,6 +28,20 @@ func _process(delta: float) -> void:
 func _on_dialogic_signal(argument:String):
 	if argument == "move_forward":
 		cutscene_animation.play("move_forward")
+	elif argument == "astra_turn1":
+		cutscene_animation.play("astra_turn1")
+	elif argument == "astra_turn2":
+		cutscene_animation.play("astra_turn2")
+	elif argument == "zoom_out":
+		cutscene_animation.play("zoom_out")
+	elif argument == "drone_in":
+		drone.visible = true
+		drone.play("idle")
+		cutscene_animation.play("drone_in")
+	elif argument == "lights_off":
+		cutscene_animation.play("lights_off")
+	elif argument == "drone_out":
+		cutscene_animation.play("drone_out")
 	
 func _on_interaction_area_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -34,6 +50,7 @@ func _on_interaction_area_body_entered(body: Node2D) -> void:
 		cutscene()
 	
 func cutscene():
+	met_cipher = true
 	print("should play animation")
 	player.set_physics_process(false)
 	player.set_process_input(false)
