6bbd1f3a93c4f5cbee6fbf67cfa3aa437b0375e9
marwa-04 <marwaelazzazi@gmail.com>
2024-12-25 17:40:35 +0200

    UI-update

---
+++ scenes/info_bubble.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func resume():
+	print("continue func")
+	get_tree().paused= false
+	
+func _on_continue_pressed() -> void:
+	print("CONTIUE")
+	resume()
