7847bb3d6617ab9039d005dfec4d00f7ec123a55
Nourin <nourinawad@gmail.com>
2025-01-09 02:57:08 +0200

    update resources

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,6 +13,5 @@ func _on_start_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_resources_pressed() -> void:
 	OS.shell_open("https://en.wikipedia.org/wiki/Internet_safety")
